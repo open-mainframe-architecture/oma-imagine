ac9ee848049c8ce1a6a8af93529b8d9d985aa87d
highdutch <dick.heijink@gmail.com>
2016-04-27 23:44:46 +0200

    es6

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
 "use strict";
 
-var path = require('path');
+const path = require('path');
 
-var constants = require('oma-constants');
-var util = require('oma-util');
+const constants = require('oma-constants');
+const util = require('oma-util');
 
-module.exports = function (bundleDirectory, bundleNames) {
-  return scanBundleReleases(bundleDirectory, bundleNames)
-    .then(function (bundleReleases) {
-      var missingBundles = bundleNames.filter(function (name) { return !bundleReleases[name]; });
-      if (missingBundles.length) {
-        throw new Error('Unknown bundle(s): ' + missingBundles.join());
-      }
-      var bestReleases = bestBundleReleases(bundleReleases);
-      var bestArchives = bestArchiveVersions(bestReleases, bundleReleases);
-      console.log(bestReleases);
-      console.log(bestArchives);
-    })
-    ;
-}
+const metaBundle = `/0/${constants.bundle.file}.json`;
+
+module.exports = (bundleDirectory, bundleNames) => scanBundleReleases(bundleDirectory, bundleNames)
+  .then(bundleReleases => {
+    const missingBundles = bundleNames.filter(name => !bundleReleases[name]);
+    if (missingBundles.length) {
+      throw new Error(`Unknown bundle(s): ${missingBundles.join()}`);
+    }
+    const bestReleases = bestBundleReleases(bundleReleases);
+    const bestArchives = bestArchiveVersions(bestReleases, bundleReleases);
+    return bestImageModules(bundleDirectory, bestReleases)
+      .then(bestModules => ({
+        constants: {
+          basename: { archive: constants.archive.file, bundle: constants.bundle.file },
+          preserve: constants.library.preserve,
+          publish: constants.library.publish
+        },
+        archives: { _: bestArchiveVersions(bestReleases, bundleReleases) },
+        bundles: { _: bestReleases },
+        modules: { _: metaModules(bestModules) }
+      }))
+      ;
+  })
+  ;
+
 
 function scanBundleReleases(bundleDirectory, bundleNames) {
-  var bundleReleases = {};
-  return util.mapFiles(bundleNames.map(function(bundleName) {
-    return bundleDirectory + '/' + bundleName + '/*/' + constants.bundle.loader + '.json';
-  }), function (metaFile, cb) {
-    util.readFileText(metaFile)
-      .then(function (jsonSource) {
-        var bundleHome = path.dirname(metaFile.path);
-        var releaseIdentity = path.basename(bundleHome);
-        var bundleName = path.basename(path.dirname(bundleHome));
-        var bundleRelease = bundleReleases[bundleName] || (bundleReleases[bundleName] = {});
-        var archiveVersions = bundleRelease[releaseIdentity] = {};
-        var metaObject = JSON.parse(jsonSource)._;
-        for (var moduleName in metaObject) {
-          var archiveVersion = metaObject[moduleName].archive;
-          archiveVersions[archiveVersion.name] = archiveVersion.version;
-        }
-        cb(null);
-      })
-    ;
-  })
-    .then(function () {
-      return bundleReleases;
-    })
+  const bundleReleases = {};
+  const files = bundleNames.map(bundleName => `${bundleDirectory}/${bundleName}/*/${metaBundle}`);
+  return util.eachFile(files, metaFile => util.readFileText(metaFile)
+    .then(jsonSource => {
+      const anonymousHome = path.dirname(metaFile.path);
+      const bundleHome = path.dirname(anonymousHome);
+      const releaseIdentity = path.basename(bundleHome);
+      const bundleName = path.basename(path.dirname(bundleHome));
+      const bundleRelease = bundleReleases[bundleName] || (bundleReleases[bundleName] = {});
+      const archiveVersions = bundleRelease[releaseIdentity] = {};
+      const metaObject = JSON.parse(jsonSource)._;
+      for (let moduleName in metaObject) {
+        const versionedArchive = metaObject[moduleName].archive;
+        archiveVersions[versionedArchive.name] = versionedArchive.version;
+      }
+    }))
+    .then(() => bundleReleases)
     ;
 }
 
 function bestBundleReleases(bundleReleases) {
-  var bestReleases = {};
-  for (var bundleName in bundleReleases) {
-    var bestReleaseIdentity = null;
-    var releases = bundleReleases[bundleName]
-    for (var releaseIdentity in releases) {
-      var thisRelease = releases[releaseIdentity];
+  const bestReleases = {};
+  for (let bundleName in bundleReleases) {
+    let bestReleaseIdentity = null;
+    const releases = bundleReleases[bundleName]
+    for (let releaseIdentity in releases) {
+      const thisRelease = releases[releaseIdentity];
       if (!bestReleaseIdentity || isBetterRelease(thisRelease, releases[bestReleaseIdentity])) {
         bestReleaseIdentity = releaseIdentity;
       }
@@ -63,13 +68,13 @@ function bestBundleReleases(bundleReleases) {
   return bestReleases;
 }
 
-function isBetterRelease(newVers, oldVers) {
-  var totalComparison = 0;
-  for (var archiveName in newVers) {
-    var newVer = newVers[archiveName], oldVer = oldVers[archiveName];
+function isBetterRelease(newVersions, oldVersions) {
+  let totalComparison = 0;
+  for (let archiveName in newVersions) {
+    const newVersion = newVersions[archiveName], oldVersion = oldVersions[archiveName];
     // if new or old version is undefined, continue with other names
-    if (newVer && oldVer) {
-      var comparison = util.compareVersions(newVer, oldVer);
+    if (newVersion && oldVersion) {
+      const comparison = util.compareVersions(newVersion, oldVersion);
       if (comparison > 0 && totalComparison >= 0) {
         // new version becomes or remains better version
         totalComparison = 1;
@@ -78,7 +83,7 @@ function isBetterRelease(newVers, oldVers) {
         totalComparison = -1;
       } else if (comparison !== 0) {
         // if versions are different, they must be consistently better or worse
-        throw new Error('Inconsistent versions: ' + archiveName + ' ' + newVer + ' & ' + oldVer);
+        throw new Error(`Inconsistent versions: ${archiveName} ${newVersion} & ${oldVersion}`);
       }
     }
   }
@@ -86,19 +91,58 @@ function isBetterRelease(newVers, oldVers) {
 }
 
 function bestArchiveVersions(bestReleases, bundleReleases) {
-  var archives = {}
-  for (var bundleName in bestReleases) {
-    var releaseIdentity = bestReleases[bundleName];
-    var archiveVers = bundleReleases[bundleName][releaseIdentity];
-    for (var archiveName in archiveVers) {
-      var thisVer = archiveVers[archiveName];
-      var existingVer = archives[archiveName];
-      if (!existingVer) {
-        archives[archiveName] = thisVer;
-      } else if (existingVer !== thisVer) {
-        throw new Error('Version conflict: ' + archiveName + ' ' + existingVer + ' & ' + thisVer);
+  const archives = {}
+  for (let bundleName in bestReleases) {
+    const releaseIdentity = bestReleases[bundleName];
+    const archiveVersions = bundleReleases[bundleName][releaseIdentity];
+    for (let archiveName in archiveVersions) {
+      const thisVersion = archiveVersions[archiveName];
+      const existingVersion = archives[archiveName];
+      if (!existingVersion) {
+        archives[archiveName] = thisVersion;
+      } else if (existingVersion !== thisVersion) {
+        throw new Error(`Version conflict: ${archiveName} ${existingVersion} & ${thisVersion}`);
       }
     }
   }
   return archives;
+}
+
+function bestImageModules(bundleDirectory, bestReleases) {
+  const modules = {};
+  return Promise.all(Object.keys(bestReleases).map(bundleName => {
+    const bundleRelease = bestReleases[bundleName];
+    const metaPath = `${bundleDirectory}/${bundleName}/${bundleRelease}/${metaBundle}`;
+    return util.openReadStream(metaPath)
+      .then(inputStream => util.readStreamText(inputStream))
+      .then(jsonSource => {
+        const metaObject = JSON.parse(jsonSource)._;
+        for (let moduleName in metaObject) {
+          if (moduleName) {
+            if (modules[moduleName]) {
+              const conflictingBundles = `${bundleName} & ${modules[moduleName].bundle}`;
+              throw new Error(`Module conflict: ${moduleName} in ${conflictingBundles}`);
+            }
+            modules[moduleName] = Object.assign({ bundle: bundleName }, metaObject[moduleName]);
+          }
+        }
+      })
+      ;
+  }))
+    .then(() => modules)
+    ;
+}
+
+function metaModules(bestModules) {
+  const modules = {};
+  for (let moduleName in bestModules) {
+    const meta = bestModules[moduleName];
+    modules[moduleName] = {
+      bundle: meta.bundle,
+      ordinal: meta.ordinal,
+      optional: meta.optional,
+      depends: meta.depends
+    };
+  }
+  return modules;
 }
